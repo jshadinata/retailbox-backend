93a3ff60c4424b0d2de62f2a0dafc8c504ae583a
Johnson Shadinata <shadinata@yahoo.com>
2019-10-17 22:57:22 +0700

    check for duplicate api key when add() and edit()

--- src/Controller/CompaniesController.php
+++ src/Controller/CompaniesController.php
@@ -11,6 +11,11 @@ class CompaniesController extends AppController {
 	
 	protected $protected_action = ['index', 'get', 'add', 'edit'];
 
+	private function setAllAndSerialize($arr) {
+		$this->set($arr);
+		$this->set('_serialize', array_keys($arr));
+	}
+
 	public function index() {
 		$user_id = $this->request->getSession()->read('User.id');
 		$companies = $this->Companies->find()			
@@ -21,8 +26,7 @@ class CompaniesController extends AppController {
 			->toArray();
 		$result = true;
 		$message = 'OK';		
-		$this->set(compact('result', 'message', 'companies'));
-		$this->set('_serialize', ['result', 'message', 'companies']);
+		$this->setAllAndSerialize(compact('result', 'message', 'companies'));		
 	}
 
 	public function get($id = null) {
@@ -42,96 +46,126 @@ class CompaniesController extends AppController {
 			->toArray();
 		$result = true;
 		$message = 'OK';		
-		$this->set(compact('result', 'message', 'company'));
-		$this->set('_serialize', ['result', 'message', 'company']);
+		$this->setAllAndSerialize(compact('result', 'message', 'company'));		
 	}
 
 	public function add() {
-		$result = false;
-		$message = 'Nothing happened.';
-		if ($this->request->is('post')) {
-			$company = $this->Companies->newEntity();
-			$company = $this->Companies->patchEntity($company, $this->request->data);			
-			if ($this->Companies->save($company)) {
-				
-				// create this user as first member
-				$this->loadModel('CompaniesUsers');
-				$user_id = $this->request->getSession()->read('User.id');
-				$cu = $this->CompaniesUsers->newEntity();
-				$cu->user_id = $user_id;
-				$cu->company_id = $company->id;
-				$cu->user_rights = ':0:';
-				$this->CompaniesUsers->save($cu);
 
-				$result = true;
-				$message = 'OK';
-				$this->set(compact('result', 'message', 'company'));
-				$this->set('_serialize', ['result', 'message', 'company']);
-			} else {
-				$message = 'The company could not be saved.';
-				$e = $company->errors();
-				$errors = [];
-				foreach ($e as $field => $msg) {
-					$errors[$field] = reset($msg);
-				}
-				$this->set(compact('result', 'message', 'errors'));
-				$this->set('_serialize', ['result', 'message', 'errors']);
-			}
-		} else {
-			$this->set(compact('result', 'message'));
-			$this->set('_serialize', ['result', 'message']);
+		// check if HTTP POST
+		if (!$this->request->is('post')) {
+			$result = false;
+			$message = 'Nothing happened.';
+			$this->setAllAndSerialize(compact('result', 'message'));
+			return;
 		}
+
+		// check duplicate api_key						
+		$proposed_api_key = $this->request->getData('api_key');
+		if ($proposed_api_key != null && $proposed_api_key != '') {
+			$query = $this->Companies->find();
+			$count = $query->select(['count' => $query->func()->count('*')])
+				->where(['api_key' => $proposed_api_key])
+				->first()->count;				
+			if ($count > 0) {
+				$result = false;
+				$errors = ['api_key' => 'Duplicate API Key, please supply another key!'];
+				$message = 'The company could not be saved.';
+				$this->setAllAndSerialize(compact('result', 'message', 'errors'));				
+				return;
+			}
+		}
+
+		// pass validation
+		$company = $this->Companies->newEntity();
+		$company = $this->Companies->patchEntity($company, $this->request->data);			
+		if ($this->Companies->save($company)) {
+			
+			// create this user as first member
+			$this->loadModel('CompaniesUsers');
+			$user_id = $this->request->getSession()->read('User.id');
+			$cu = $this->CompaniesUsers->newEntity();
+			$cu->user_id = $user_id;
+			$cu->company_id = $company->id;
+			$cu->user_rights = ':0:';
+			$this->CompaniesUsers->save($cu);
+
+			$result = true;
+			$message = 'OK';
+			$this->setAllAndSerialize(compact('result', 'message', 'company'));
+			
+		} else {
+			$message = 'The company could not be saved.';
+			$e = $company->errors();
+			$errors = [];
+			foreach ($e as $field => $msg) {
+				$errors[$field] = reset($msg);
+			}
+			$this->setAllAndSerialize(compact('result', 'message', 'errors'));			
+		}		
 	}
 
 	public function edit($id) {
-		$result = false;
-		$message = 'Nothing happened.';
-		if ($this->request->is('post')) {
 
-			// check if user is owner (has :0: key)
-			$user_id = $this->request->getSession()->read('User.id');
-			$this->loadModel('CompaniesUsers');
-			$uc = $this->CompaniesUsers->find()
-				->select('user_rights')
+		// check if HTTP POST
+		if (!$this->request->is('post')) {
+			$result = false;
+			$message = 'Nothing happened.';
+			$this->setAllAndSerialize(compact('result', 'message'));	
+			return;
+		}
+
+		// check duplicate api_key						
+		$proposed_api_key = $this->request->getData('api_key');
+		if ($proposed_api_key != null && $proposed_api_key != '') {
+			$query = $this->Companies->find();
+			$count = $query->select(['count' => $query->func()->count('*')])
 				->where([
-					'user_id' => $user_id,
-					'company_id' => $id,
+					'id !=' => $id,
+					'api_key' => $proposed_api_key
 				])
-				->first();
-
-			if ($uc == null || strpos($uc['user_rights'], ':0:') === false) {
-
-				// don't has owner key
+				->first()->count;				
+			if ($count > 0) {
 				$result = false;
-				$message = 'Only owner can edit company info.';
-				$this->set(compact('result', 'message'));
-				$this->set('_serialize', ['result', 'message']);
-			} else {
-
-				// has owner key
-				$company = $this->Companies->get($id, ['contain'=>[]]);
-				$company = $this->Companies->patchEntity($company, $this->request->data);
-				if ($this->Companies->save($company)) {
-					$result = true;
-					$message = 'OK';				
-					$this->set(compact('result', 'message', 'company'));
-					$this->set('_serialize', ['result', 'message', 'company']);
-				} else {
-					$message = 'The company could not be saved.';
-					$e = $company->errors();
-					$errors = [];
-					foreach ($e as $field => $msg) {
-						$errors[$field] = reset($msg);
-					}
-					$this->set(compact('result', 'message', 'errors'));
-					$this->set('_serialize', ['result', 'message', 'errors']);
-				}
+				$errors = ['api_key' => 'Duplicate API Key, please supply another key!'];
+				$message = 'The company could not be saved.';
+				$this->setAllAndSerialize(compact('result', 'message', 'errors'));					
+				return;
 			}
-		} else {
+		}
 
-			// not a HTTP POST request
-			$this->set(compact('result', 'message'));
-			$this->set('_serialize', ['result', 'message']);	
+		// check if user is owner (has :0: key)
+		$user_id = $this->request->getSession()->read('User.id');
+		$this->loadModel('CompaniesUsers');
+		$uc = $this->CompaniesUsers->find()
+			->select('user_rights')
+			->where([
+				'user_id' => $user_id,
+				'company_id' => $id,
+			])
+			->first();
+		if ($uc == null || strpos($uc['user_rights'], ':0:') === false) {
+			$result = false;
+			$message = 'Only owner can edit company info.';
+			$this->setAllAndSerialize(compact('result', 'message'));				
+			return;
+		} 
+
+		// pass validation
+		$company = $this->Companies->get($id, ['contain'=>[]]);
+		$company = $this->Companies->patchEntity($company, $this->request->data);
+		if ($this->Companies->save($company)) {
+			$result = true;
+			$message = 'OK';				
+			$this->setAllAndSerialize(compact('result', 'message', 'company'));				
+		} else {
+			$result = false;
+			$message = 'The company could not be saved.';
+			$e = $company->errors();
+			$errors = [];
+			foreach ($e as $field => $msg) {
+				$errors[$field] = reset($msg);
+			}
+			$this->setAllAndSerialize(compact('result', 'message', 'errors'));
 		}
 	}
 
